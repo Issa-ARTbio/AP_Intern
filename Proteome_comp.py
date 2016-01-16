dfc6652fe52c1c625ce470f81e7de0b8557ac812
Issa_d <diop.seydina@laposte.net>
2016-01-16 01:17:40 +0100

    Le nombre et le pourcentage des acides aminés dans chaque proteome

--- Proteome_comp.py
+++ Proteome_comp.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
-import os.path
-import sys
-
-''' Pour chaque protome dans le dossier, renvoie le nombre et et pourcentage des acides aminés
+''' Pour chaque protome dans le dossier, renvoie le nombre et pourcentage des acides aminés
 
 '''
 
@@ -13,11 +9,9 @@ import sys
 import os
 import os.path
 import sys
-from path import path
 
 def comp_proteome (proteome_dir, list_aa):
     Taille_proteome = 0 # Initialise la taille de chaque protéome
-    # output = open(output_file, 'w')
     with open(proteome_dir) as f:
         Prot = {}
         for line in f:
@@ -28,14 +22,12 @@ def comp_proteome (proteome_dir, list_aa):
                     Prot[aa] = Prot.get(aa, 0) +1 #Ajoute 0 si la clé est absente et 1 si elle est présente dans le dico Prot
         line = os.path.basename(proteome_dir)+ '\t'
         for aa in list_aa:
-            line += str(Prot.get(aa, 0)) # Ajoute le nombre total de l'aa s'il est présente 0
+            line += str(Prot.get(aa, 0)) # Ajoute le nombre total de l'aa s'il est présente, sinon, renvoie 0 et le creer
             percentage = Prot.get(aa, 0)*100 / Taille_proteome
             line = line + ' ' + str(percentage) + '\t'
         print (line)
-        # output.write(line)
-        # output.close()
+    return (line)
 
-# output_file = '/home/issa/Documents/STAGE/Data/Données_Initiales/Proteomes_test/'.
 proteome_dir = '/home/issa/Documents/STAGE/Data/Données_Initiales/Proteomes_test/'
 list_aa = ['A', 'R', 'N', 'D', 'C', 'Q', 'E','G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
 print ( 'Proteome\t'+"\t".join(list_aa) )
@@ -44,4 +36,3 @@ for filename in os.listdir(proteome_dir):
         path_proteome = os.path.join(proteome_dir, filename)
         comp_proteome(path_proteome, list_aa)
 
-# proteome_file = os.listdir(proteome_dir)
