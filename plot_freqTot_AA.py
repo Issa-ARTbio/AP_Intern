6d6d348526dae4c833f9ee235ae4354da748827b
IssaARTbio <diop.seydina@laposte.net>
2016-01-29 18:04:34 +0100

    add python script

---
+++ plot_freqTot_AA.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''plot sur une meme figure la frequence de chaque aa contenue dans les proteomes
+-i(file.dat)
+
+
+usage:
+=====
+$ python3 plot_freqTot_AA.py -i(.dat)
+'''
+
+import os, sys, argparse, re
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import cycle
+from collections import OrderedDict
+
+def Freq_AA (proteom_composition_aa):
+    """ 'Description de la fonction' : A partir du fichier Taille_Proteome.dat contenant la composition en acide amine pour chaque proteome
+    renvoit un plot de la frequence de chaque acide amine sur les 110 proteomes
+
+    Parameters:
+    ===========
+    proteom_composition_aa: file
+        un fichier avec les 20 acides amines en colonnes separes par '\ŧ' en premiere colonne = nom des proteomes
+        le nombre et le pourcentage de chaque aa en ligne separe par un space
+
+    outf: file for each aa
+        plot de la frequence de chaque aa dans les differents proteomes """
+
+    dico_freq = dict()
+    with open(proteom_composition_aa) as f:
+        line = f.readline()
+        line = line.strip()
+        liste_aa = line.split('\t')[1:]
+        for line in f:
+            line = line.strip()
+            tmp = line.split('\t')
+            proteome_name = tmp[0]
+            proteome_name = proteome_name.split('.')[0]
+            # print(proteome_name)
+            values = tmp[1:]
+            for i, val in enumerate (values):
+                dico_freq.setdefault (liste_aa[i], dict())
+                dico_freq[liste_aa[i]][proteome_name] = (float(val.split()[1]))/100
+
+    N = 111
+    n = 20
+    ind = np.arange(N)
+    indp= np.arange(n)
+    datas= []
+    d=[]
+    # fig = plt.figure()
+    width = 0.35
+    labels_aa= []
+    labels_prt = []
+    my_handle = []
+    for aa in dico_freq.keys():
+        labels_aa.append(aa)
+        data = dico_freq[aa]
+        datas.append(data)
+        values_aa = []
+        # linestyles = {'1': 'solid', '2': 'dashed', '3': 'dashdot', '4': 'dotted'}
+        # markers = {'d': 'thin_diamond', '*': 'star', '^': 'triangle_up', '+': 'plus', 's': 'square'
+        # color = {}
+        for proteome in data.keys():
+            # mk = markers[m]
+            data_aa=(data[proteome])
+            values_aa.append(data_aa)
+            # print(ind, values_aa)
+            d.append(values_aa)
+            values_aa = sorted(values_aa)
+            labels_prt.append(proteome)
+        zipper = list(zip(labels_prt, values_aa))
+        zipper_sort = sorted (zipper)
+        key_trie, val_trie = zip(*zipper_sort)
+
+        petit = ['S', 'T','C','P']
+        apolaire = ['A', 'G' , 'V', 'L', 'M', 'I', 'F', 'Y', 'W']
+        polaire = ['D', 'E', 'K', 'R', 'H', 'N', 'Q']
+        # filled_markers = ('o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd')
+
+        if aa in apolaire:
+            my_handle.append(aa)
+            if aa == 'A': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= '>', label= 'apolaire (A)')#, zorder=1)
+            elif aa == 'G': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= '*', label= 'apolaire (G)')#, zorder=1)
+            elif aa == 'V': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= 'v', label= 'Aliphatique (V)')#, zorder=1)
+            elif aa == 'L': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= 'd', label= 'Aliphatique (L)')#, zorder=1)
+            elif aa == 'M': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= 'p', label= 'Hydrophobe fort(M)')#, zorder=1)
+            elif aa == 'I': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= '1', label= 'Aliphatique (I)')#, zorder=1)
+            elif aa == 'F': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= '^', label= 'aromatique (F)')#, zorder=1)
+            elif aa == 'Y': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= 's', label= 'aromatique (Y)')#, zorder=1)
+            elif aa == 'W': plt.plot(ind, values_aa, linewidth = 2 , color = 'purple',marker= 'h', label= 'aromatique (W)')#, zorder=1)
+        elif aa in polaire:
+            my_handle.append(aa)
+            if aa == 'K': plt.plot(ind, values_aa, linewidth = 2 , color = 'green',marker= 'o', label= 'Polaire charge positif (K)')#, zorder=2)
+            elif aa == 'R': plt.plot(ind, values_aa, linewidth = 2 , color = 'green',marker= '*', label= 'Polaire charge positif (R)')#, zorder=2)
+            elif aa == 'H': plt.plot(ind, values_aa, linewidth = 2 , color = 'green',marker= 'v', label= 'Polaire charge positif (H)')#, zorder=2)
+            elif aa == 'D': plt.plot(ind, values_aa, linewidth = 2 , color = 'green',marker= '>', label= 'Polaire charge negatif (D)')#, zorder=2)
+            elif aa == 'E': plt.plot(ind, values_aa, linewidth = 2 , color = 'green',marker= '<', label= 'Polaire charge negatif (E)')#, zorder=2)
+            elif aa == 'N': plt.plot(ind, values_aa, linewidth = 2 , color = 'green',marker= 's', label= 'polaire non charge (N)')#, zorder=2)
+            elif aa == 'Q': plt.plot(ind, values_aa, linewidth = 2 , color = 'green',marker= 'd', label= 'polaire non charge (Q)')#, zorder=2)
+        elif aa in petit:
+            my_handle.append(aa)
+            if aa == 'S': plt.plot(ind, values_aa, linewidth = 2 , color = 'r',marker= 'o', label= 'polaire non charge (S)', zorder=3)
+            elif aa == 'T': plt.plot(ind, values_aa, linewidth = 2 , color = 'r',marker= '*', label= 'polaire non charge (T)', zorder=2)
+            elif aa == 'C': plt.plot(ind, values_aa, linewidth = 2 , color = 'r',marker= 'v', label= 'polaire non charge (C)', zorder=4)
+            elif aa == 'P': plt.plot(ind, values_aa, linewidth = 2 , color = 'r',marker= '|', label= 'apolaire (P)', zorder=1)
+        plt.legend (my_handle,ncol = 3)
+
+        f = (ind+width)
+        f = sorted (f)
+        plt.grid(True)
+        plt.xlabel('Proteomes', fontsize=15, color='blue')
+        plt.ylabel('Frequence', fontsize=15, color='red')
+        plt.title('Distribution de la Frequence des aa dans les Proteomes', fontdict={'family': 'monospace'})
+        plt.xticks(f , fontsize=2)
+    my_handle, labels = plt.gca().get_legend_handles_labels()
+    zipper_leg = list(zip(labels, my_handle))
+    zipper_leg_sort = sorted (zipper_leg)
+    lab, h_aa = zip(*zipper_leg_sort)
+    print (lab)
+    l = plt.legend(h_aa, lab, ncol=3, loc='upper left', fontsize=8)
+    l.set_zorder(20)
+    plt.show()
+    # plt.savefig(aa+u'_Freqence.pdf')
+
+
+proteom_composition_aa = '/home/issa/Documents/STAGE/Results/proteome_composition_AA.csv'
+frequence_aa = Freq_AA (proteom_composition_aa)
---
+++ polar_plot.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Renvoit deux polar_plots pour les frequences borders et les frequence bio
+"""
+
+import os, sys, argparse, re
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+def read_freq (proteom_composition_aa):
+    """ 'Description de la fonction' : A partir du fichier Taille_Proteome.dat contenant la composition en acide amine pour chaque proteome
+    renvoit un dict{ aa, {data_aa}}
+
+    """
+
+    dico_freq = dict()
+    with open(proteom_composition_aa) as f:
+        line = f.readline()
+        line = line.strip()
+        liste_aa = line.split('\t')[1:]
+        for line in f:
+            line = line.strip()
+            tmp = line.split('\t')
+            proteome_name = tmp[0]
+            proteome_name = proteome_name.split('.')[0]
+            # print(proteome_name)
+            values = tmp[1:]
+            for i, val in enumerate (values):
+                dico_freq.setdefault (liste_aa[i], dict())
+                dico_freq[liste_aa[i]][proteome_name] = (float(val.split()[1]))/100
+
+    return (dico_freq)
+
+def get_all_freq(proteome_name, dico_freq, list_aa):
+    """ return the list of AA frequencies for a particular proteome
+    """
+    list_freq = []
+
+    for aa in list_aa:
+        fr_aa = dico_freq[aa][proteome_name]
+        list_freq.append(fr_aa)
+
+    pass
+
+
+
+def get_name_min_med_max (dico_freq, amino, list_aa):
+    """renvoit le nom du proteome avec la frequence minimale de chaque aa present dans amino, avec une grande variation de la frequence de
+    Minimale, mediane, maximale
+    """
+    freq_All_min, freq_All_med, freq_All_max = [], [], []
+
+    my_dico_min = {}
+    my_dico_med = {}
+    my_dico_max = {}
+
+    for aa in amino:
+
+        my_dico_min[aa] = []
+        my_dico_med[aa] = []
+        my_dico_max[aa] = []
+        # name_min_proteome, name_med_proteome, name_max_proteome = None, None, None
+        data_aa = dico_freq[aa]
+        proteome_name, values_aa = zip(*data_aa.items())
+        zipper = list(zip(values_aa, proteome_name))
+        zipper_sort = sorted(zipper)
+        val_trie, key_trie = zip(*zipper_sort)
+        # print (key_trie , val_trie)
+        medium = len(val_trie)/2 + 0.5
+        name_min_proteome = key_trie [0]
+        my_dico_min[aa].append(name_min_proteome)
+
+        name_med_proteome = key_trie[int(medium)]
+        my_dico_med[aa].append(name_med_proteome)
+
+        name_max_proteome = key_trie [-1]
+        my_dico_max[aa].append(name_max_proteome)
+
+        for aa2 in list_aa:
+
+            if name_min_proteome in data_aa:
+                fr = dico_freq[aa2][name_min_proteome]
+                my_dico_min[aa].append(fr)
+
+            if name_med_proteome in data_aa:
+                fr = dico_freq[aa2][name_med_proteome]
+                my_dico_med[aa].append(fr)
+
+            if name_med_proteome in data_aa:
+                fr = dico_freq[aa2][name_max_proteome]
+                my_dico_max[aa].append(fr)
+    # print ('my_dico_min', my_dico_min, 'my_dico_med',my_dico_med,  'my_dico_med', my_dico_med)
+
+    return my_dico_min, my_dico_med,  my_dico_max
+
+
+def polar_plotting (my_dico_min, my_dico_med,  my_dico_max, list_aa):
+    '''renvoit un plot de la Frequence des differents aa dans les proteomes
+    '''
+
+    # fig = super(PlotWindPowerDensity, self).get_figure()
+    for aa in my_dico_min:
+        # if aa == 'L':
+        data_min,  data_med, data_max = None, None, None
+        liste_min, liste_med, liste_max = [], [], []
+
+        data_min = my_dico_min[aa]
+        liste_min.append(data_min)
+        name_min_proteome = liste_min[0]
+
+
+        data_med = my_dico_med[aa]
+        liste_med.append(data_med)
+        name_med_proteome = liste_med[0]
+
+        data_max = my_dico_max[aa]
+        liste_max.append(data_max)
+        name_max_proteome = liste_max[0]
+
+        ax = plt.subplot(111, polar=True)
+
+
+
+        # print (data_min_s, list_aa)
+        r = np.arange(len(list_aa))
+        rad = (r*2*np.pi/len(r))
+        rad = list(rad)
+        rad.append(rad[0])
+        # ax.plot(data,  color='r',linewidth=1,alpha= 0.5, label = data_min[0], marker= '*')
+        # N = 20
+        # theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+        # fig = ax.bar(r, data_min_s,  color='r',linewidth=5,alpha= 0.5, label = data_min[0], )
+        # i = float([0])
+        data_min_s = list(data_min[1:])
+        data_min_s.append(data_min_s[0])
+
+        data_med_s = list(data_med[1:])
+        data_med_s.append(data_med_s[0])
+
+        data_max_s = list(data_max[1:])
+        data_max_s.append(data_max_s[0])
+
+        ax.plot(rad, data_min_s,  color='r',linewidth=5,alpha= 0.3, label = data_min[0]+'\n Minimale', marker= '*')
+        ax.fill (rad, data_min_s, color= 'r', alpha= 0.5)
+
+        ax.plot(rad, data_med_s,  color='b',linewidth=5,alpha= 0.3, label = data_med[0]+'\n Mediane', marker= 's')
+        ax.fill (rad, data_med_s, color= 'b', alpha= 0.5)
+
+        ax.plot(rad, data_max_s,  color='g',linewidth=5,alpha= 0.7, label = data_max[0]+'\n Maximale', marker= '>')
+        ax.fill (rad, data_max_s,color= 'g', alpha= 0.5)
+
+        plt.grid(True)
+        rad = rad[:-1]
+        ax.set_xticks(rad)
+        ax.set_rmax(0.15)
+        # ax.set_xticklabels (list_aa) #, color= 'b') #for i in ['A', 'K', 'N', 'L'] )
+        hydrophobe = (['V', 'I', 'L', 'M', 'F', 'W', 'Y'])
+        polaire = (['K', 'R', 'H', 'D', 'E', 'N', 'S', 'Q',  'T'])
+        petit_apolaire = (['P', 'G', 'A', 'C'])
+        list_lab = (hydrophobe, polaire, petit_apolaire)
+        colors = [ 'b', 'm', 'grey']
+        for xtick, i in zip (ax.get_xticklabels(), list_aa):
+
+            if i in hydrophobe:
+                xtick.set_color(colors[0])
+            if i in polaire:
+                xtick.set_color(colors[1])
+            if i in petit_apolaire:
+                xtick.set_color(colors[2])
+                ax.set_xticklabels(list_aa)
+        ax.set_title('Frequence des differents aa dans les proteomes\n avec une grande variation de la frequence de '+aa, va ='bottom', color = 'k', fontdict={'family': 'monospace'})
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.10),
+  fancybox=True, shadow=True, ncol=3)
+        # , label= 'b' if i == rad [0] else '')
+        # fig.add_axes(ax)
+        plt.show()
+
+
+
+if __name__ == "__main__":
+    proteom_composition_aa = 'proteome_composition_AA.csv'
+    dico_aa = read_freq (proteom_composition_aa)
+    amino = ['A', 'K', 'N', 'L']
+    list_aa = ['V', 'I', 'L', 'M', 'F', 'W', 'Y', 'K', 'R', 'H', 'D', 'E', 'N', 'S', 'Q',  'T', 'P', 'G', 'A',
+     'C']
+    dico_min, dico_med, dico_max = get_name_min_med_max(dico_aa,amino, list_aa)
+    my_plot = polar_plotting(dico_min, dico_med, dico_max, list_aa)
+    # polar_plotting (dico_aa, names)
