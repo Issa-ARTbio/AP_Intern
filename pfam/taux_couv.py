627de5a4a4568f672cedda75e6df9f172455adf8
Issa DIOP <diop.seydina@laposte.net>
2016-02-10 16:16:16 +0100

    done

--- pfam/taux_couv.py
+++ pfam/taux_couv.py
@@ -3,12 +3,13 @@
 
 
 ''' Pour chaque proteome:
-    calcul le taux de couverture des domaines
-    et la frequence des proteines avec au moins 1 domaine present
+    calcul le taux de couverture en residues
+    et le taux de couverture en domaine.
+    renvoit un histogramme du taux de coubverture
 
 Parametres:
 
--i dir (.fasta) dir (.pfam) -o []
+-i dir (.fasta) dir (.pfam) -o [histogramme]
 
 
 usage
@@ -62,7 +63,7 @@ def read_pfam_outp (path_pfam_ouf):
 
 def long_couvert(pfam_dict):
     '''Description de la fonction: renvoit la longeur totale couvert par les domaines sur la proteine
-    :NB: permet de passer de la redondance des protein_id de la fonction precedente à un seul id par protein et comme value : la couverture total des domaine/protein
+    :NB: permet de passer de la redondance des protein_id de la fonction precedente à un seul id par protein et comme value : la couverture totale des domaine/protein
     '''
     len_domains = {}
     for seq_id in pfam_dict:
@@ -73,12 +74,12 @@ def long_couvert(pfam_dict):
     return len_domains
 
 def taux_couverture(directory, liste_fasta, pfam_out, list_pfam_out):
-    '''Description de la fonction: prend comme entrees deux dico avec les mm keys = proteome name
-    dans proteome_dict : values = dict {'protein_name': seq_len}
-    dans all_domains: values = dict {'protein_name': len_domain}
+    '''Description de la fonction:
+    prend comme entrees la localisation et la liste des fichiers fasta et pfam.
+    execute les deux fonctions (read_pfam_outp) et (long_couvert)
     renvoit:
-    Taux de couvertures pour les proteomes
-    Et la frequence de protein(ayant au moins un domaine) sur le nombre total de protein
+    Taux de couverture pour les proteomes en residus (len_domain_concat/len_totale_proteome)*100
+    Et le taux de couverture de chaque proteome en domain (ayant au moins un domaine present) = (nmb_domain_present/nombre_total_protein)*100
     '''
 
     all_prot, all_dom = dict(), dict()
@@ -121,68 +122,74 @@ def plotting(all_prot, all_dom):
     '''renvoit un histogramme pour le taux de couverture en residue
     et pour le taux de couverture en domaines de chaque proteome
     '''
+
+    all_couv = []
+    t_prot_dom_name=()
+    for proteome_name in all_prot:
+
+
+        taux_prt = all_prot[proteome_name]
+        taux_dom = all_dom[proteome_name]
+        t_prot_dom_name = taux_prt, taux_dom, proteome_name
+
+        all_couv.append(t_prot_dom_name)
+
+    all_couv.sort()
+
     list_de_taux = []
     list_domain1 = []
     list_prot_name = []
+    for data_protein, data_dom, proteome_lab in all_couv:
 
-    for proteome_name in all_prot:
+        list_de_taux.append(data_protein)
+        list_domain1.append(data_dom)
+        list_prot_name.append(proteome_lab)
+    # pour creer des partitions
+    # for i in range(0, len(list_prot_name), 30):
+    #     data_protein = list_de_taux[i : i +30]
+    #     data_domain = list_domain1[i : i +30]
+    #     name_proteome = list_prot_name[i : i +30]
 
-        list_prot_name.append(proteome_name)
-
-        taux_prt = all_prot[proteome_name]
-        list_de_taux.append(taux_prt)
-
-        taux_dom = all_dom[proteome_name]
-        list_domain1.append(taux_dom)
 
     fig, ax = plt.subplots()
-    N = 111
+    N = len(list_prot_name)
     ind = np.arange(N)
     width = 0.35
 
-
-    zipper = list(zip(list_de_taux, list_prot_name))
-    zipper_sort = sorted (zipper)
-    data_prot, proteome_lab = zip(*zipper_sort)
-    prot = ax.bar(ind, data_prot, width=0.2, alpha=0.5, color='b', label= 'Taux de couverture en residues (%)')
-
-
-    zipper = list(zip(list_domain1, list_prot_name))
-    zipper_sort = sorted (zipper)
-    data_dom, proteome_nam = zip(*zipper_sort)
-    dom = ax.bar(ind+width, data_dom, width=0.2, alpha=0.5, color='g', label= 'Taux de couverture en domaines (%)')
-
+    prot = ax.bar(ind, list_de_taux, width=0.2, alpha=0.5, color='b', label= 'Taux de couverture en residues (%)')
+    dom = ax.bar(ind+width, list_domain1, width=0.2, alpha=0.5, color='g', label= 'Taux de couverture en domaines (%)')
+    #
     ax.set_xlim(-width,len(ind)+width)
-
     ax.set_xticks(ind+width)
-    ax.set_xticklabels (proteome_lab, rotation='vertical', fontsize=8)
-    for i in ax.set_xticklabels(proteome_lab, rotation= 'vertical'):
-        if re.search('Synechococcus_sp_PCC_6312', str(i)) :
-            i.set_color('green')
-        elif re.search('Synechococcus_calcipolaris' , str(i)):
-            i.set_color('green')
-        elif re.search('Thermosynechococcus_elongatus_BP1' , str(i)):
-            i.set_color('green')
+    ax.set_xticklabels (list_prot_name, rotation='vertical', fontsize=10)
+    for name in ax.set_xticklabels(list_prot_name, rotation= 'vertical', fontsize=10):
+        if re.search('Synechococcus_sp_PCC_6312', str(name)) :
+            name.set_color('green')
+        elif re.search('Synechococcus_calcipolaris' , str(name)):
+            name.set_color('green')
+        elif re.search('Thermosynechococcus_elongatus_BP1' , str(name)):
+            name.set_color('green')
 
 
-        elif re.search('Gloeomargarita_lithophora' , str(i)):
-            i.set_color('red')
-        elif  re.search ('Cyanothece_sp_PCC_7425', str(i)):
-            i.set_color('red')
-        elif  re.search ('Chroococcidiopsis_thermalis_PCC_7203', str(i)):
-            i.set_color('red')
+        elif re.search('Gloeomargarita_lithophora' , str(name)):
+            name.set_color('red')
+        elif  re.search ('Cyanothece_sp_PCC_7425', str(name)):
+            name.set_color('red')
+        elif  re.search ('Chroococcidiopsis_thermalis_PCC_7203', str(name)):
+            name.set_color('red')
     plt.grid(True)
     plt.ylabel('Taux de couverture', fontsize=15, color='g', alpha=0.8)
     plt.xlabel('Proteomes', fontsize=15, color='b', alpha=0.8)
+    ax.set_ylim(0,100)
     plt.title(u"Taux de couverture des proteomes par Pfam", fontsize=17, fontdict={'family': 'monospace'})
-    ax.legend(loc='upper left')
-    # plt.savefig(filename+u'boxplotProteome.png')
+    ax.legend(loc='upper left', fontsize=10)
+    # # plt.savefig(filename+u'boxplotProteome.png')
     plt.show()
 
 
 if __name__ == '__main__':
 
-    directory = '/home/issa/Documents/STAGE/Init_data/proteomes'
+    directory = '/home/issa/Documents/stage/init_data/proteomes'
     liste_fasta = os.listdir(directory)
 
     pfam_out = '/home/issa/pfam/pfamScan/results_pfamscan'
