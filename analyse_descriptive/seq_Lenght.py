1035c658aa6cc90082483efe677e566a29a111bc
Issa DIOP <diop.seydina@laposte.net>
2016-03-17 12:45:03 +0100

    Clean repositories

---
+++ analyse_descriptive/seq_Lenght.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" renvoit un fichier de sortie'.dat' avec 3 colonnes( Nom du Proteome - Nom Protein - Len Protein)
+"""
+
+import os, sys, argparse
+from Box_plot_P20 import read_fasta
+
+def protein_name_lenght (proteome_dir, out_file):
+    with open(out_file, 'w') as outf:
+        for filename in list_proteome_name:
+            if filename.endswith('.fasta'):
+                path_proteome = os.path.join(proteome_dir, filename)
+                filename = filename.split('.')[0]
+                list_proteome_name.append(filename)
+                dico_protein = read_fasta(path_proteome)
+
+                for prot in dico_protein :
+                    seq = dico_protein[prot]
+                    lenght_seq = len(seq)
+                    outf.write( filename + '\t' +  prot + '\t' + str(lenght_seq) + '\n')
+proteome_dir = '/home/issa/Documents/STAGE/Data/Init_data/proteomes/'
+list_proteome_name = os.listdir(proteome_dir)
+out_file = 'proteome_protein_name_lenght.dat'
+
+protein_name_lenght(proteome_dir,  out_file)
